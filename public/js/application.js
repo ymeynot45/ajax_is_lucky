3318670d24cc3f58060fd920d39d9233a9776141
William Karsten <yme_ynot45@hotmail.com>
2013-07-19 18:31:21 -0500

    it now works with only ajax

--- public/js/application.js
+++ public/js/application.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
-  $(form).on('submit', function(event){
+  $('form').submit(function(event){
     event.preventDefault();
-    var value = 3.serialize();
-    $.post('/rolls'), value, function(response){
-      alert(response);
-    }
-  })
+    $.post(('/rolls'), { "value": 3 }, function(response){
+      var newDie = $(response).find('img');
+      $(document).find('#die').html(newDie[0]);
+    });
+  });
+});
 
   // PSEUDO-CODE:
   //   1- intercept the form submission event using jQuery
@@ -13,5 +14,3 @@ $(document).ready(function () {
   //   3- generate a random number between 1 and 6 using JavaScript
   //   4- use jQuery to submit an AJAX post to the form's action
   //   5- when the AJAX post is done, replace the contents of the "#die" DIV in the DOM using jQuery
-
-});
